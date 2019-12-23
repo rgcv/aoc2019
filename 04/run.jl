583c14d2b4ec2e0fa6f4b6211213560a3f2f10f7
Rui Ventura <rventura.pt@outlook.com>
2019-12-23 17:01:31 +0000

    Day 4 part 2
    Includes some fun with currying, regex, and what not

--- 04/run.jl
+++ 04/run.jl
@@ -2,11 +2,19 @@ const range = UnitRange(parse.(Int, split("357253-892942", '-'))...)
 
 diffs(a) = [a[i + 1] - a[i] for i in 1:length(a) - 1]
 
+has_double(password::AbstractString) = any(iszero, diffs(password))
+has_strict_double(password::AbstractString) =
+    any(isequal(2), map(length, findall(r"(?:(\d)\1*)", password)))
+
 criteria(password::Int) = criteria(string(password))
-criteria(password::AbstractString) =
-    all([length(password) == 6,
-         issorted(password),
-         any(isequal(0), diffs(password))])
+criteria(password::AbstractString) = criteria(has_double, password)
+
+criteria(p::Function) = password -> criteria(p, password)
+criteria(p::Function, password::Int) = criteria(p, string(password))
+criteria(p::Function, password::AbstractString) =
+    all([length(password) == 6, issorted(password), p(password)])
 
 println("""--- Part One ---
            # of Different Passwords: $(count(criteria, range))""")
+println("""--- Part Two ---
+           # of Different Passwords: $(count(criteria(has_strict_double), range))""")
