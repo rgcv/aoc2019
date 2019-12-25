0d65b76759046852b2cf5bfd6b1771398b5da4b5
Rui Ventura <rventura.pt@outlook.com>
2019-12-25 06:50:45 +0000

    Proper output in day 5 part 1

--- 05/run.jl
+++ 05/run.jl
@@ -5,5 +5,7 @@ const input = joinpath(@__DIR__, "input.txt")
 
 let p = Program(input)
     setinput!(p, () -> 1)
-    @show intcode!(p)
+    println("""--- Part One ---
+               Diagnostic code at the end""")
+    intcode!(p)
 end
